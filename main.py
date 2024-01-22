6bdaf98a0b70c5d3c8fe1031dadd236291a6e30d
Sergey Asriyants <asriyants.ser@gmail.com>
2024-01-23 02:00:57 +0300

    init rabbit service

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from twisted.internet import endpoints, reactor
 from mediator.setup import mediator_set_up
 from protocol.factory.implementation import LighterFactory
 from protocol.service.implementation import LighterService
-
+from rabbit.launch import start_rabbit
 
 application = service.Application("Lighter")
 ls = LighterService()
@@ -17,4 +17,5 @@ ws_endpoint = endpoints.serverFromString(reactor, 'tcp:8000')
 ws_endpoint.listen(ws_factory)
 
 mediator_set_up()
+start_rabbit(application)
 reactor.run()
---
+++ rabbit/launch.py
@@ -0,0 +1,23 @@
+import os
+
+import pika
+from rabbit.publisher.service import RabbitPublisherService
+from rabbit.service import PikaService
+
+
+def start_rabbit(application):
+    ps = PikaService(
+        pika.ConnectionParameters(
+            host=os.getenv("RABBITMQ_HOST", "127.0.0.1"),
+            port=os.getenv("RABBITMQ_PORT", 5672),
+            virtual_host="/",
+            credentials=pika.PlainCredentials(os.getenv("RABBITMQ_USER", "guest"), os.getenv("RABBITMQ_PASS", "guest"))
+        )
+    )
+
+    ps.setServiceParent(application)
+    ps.startService()
+
+    rps = RabbitPublisherService()
+    rps.setServiceParent(application)
+    rps.startService()
